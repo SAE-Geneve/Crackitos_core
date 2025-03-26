b7fec6ee1647701a994c9b136f2754310535889a
materfe <mathieurouxdasilva2005@gmail.com>
2025-03-26 14:51:33 +0100

    ok i am trying

---
+++ core/core/common/include/commons.h
@@ -0,0 +1,17 @@
+#ifndef CRACKITOS_PHYSICS_COMMON_COMMONS_H_
+#define CRACKITOS_PHYSICS_COMMON_COMMONS_H_
+
+namespace crackitos_core::commons
+{
+    using fp = float;
+    constexpr static fp kPi = 3.14159265358979323846f;
+    constexpr static fp kEpsilon = 0.000001f;
+
+
+    template <typename T>
+    [[nodiscard]] constexpr bool Approx(T value, T target)
+    {
+        return abs(value - target) <= kEpsilon;
+    }
+} // namespace commons
+#endif // CRACKITOS_PHYSICS_COMMON_COMMONS_H_
