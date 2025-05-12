4f61445badb1e7e54f804d81023e9862f0d6937f
mebearwhodis <144151605+mebearwhodis@users.noreply.github.com>
2025-05-12 15:02:48 +0200

    added sixit and changed CMakeLists

--- core/common/include/commons.h
+++ core/common/include/commons.h
@@ -1,8 +1,10 @@
 #ifndef CRACKITOS_PHYSICS_COMMON_COMMONS_H_
 #define CRACKITOS_PHYSICS_COMMON_COMMONS_H_
+#include "dmath/gamefloat/ieee_float_inline_asm.h"
 
 namespace crackitos_core::commons
 {
+    // using fp = sixit::dmath::ieee_float_inline_asm;
     using fp = float;
     constexpr static fp kPi = 3.14159265358979323846f;
     constexpr static fp kEpsilon = 0.000001f;
